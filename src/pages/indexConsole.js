9bb5b854872100fb7502e4b15f7c90781965c045
TBS093A <zukkamil.44@gmail.com>
2020-05-13 17:33:32 +0200

    Initial commit

---
+++ src/pages/indexConsole.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react'
+
+import '../styles/general.scss'
+
+const indexConsole = ({ }) => {
+    
+    return (
+
+        <div>
+            <div>
+                Ładowanie funkcjonalności / загрузка контента с функциональностью сайта
+            </div>
+            <div>
+                Łączenie z serwerem / подключение к серверу
+            </div>
+            <div>
+                Arłamów ============================ Петропавловск-Камчатский
+            </div>
+            <div className='ASCIIview'>
+                <div className='ASCIIchars' id='Poland'>
+            =-¬¬   TF▄                              <br />
+           .⌐          ,   ..,                      <br />
+  ▌φ^'`                          `'  ¬---┬  ,       <br />
+  Φ¬¬                                        '      <br />
+  ▄                                           W     <br />
+ ,`                                           ▐     <br />
+',                                             ⌐    <br />
+  #                                            ▀    <br />
+  ▀                                            ,¬   <br />
+  ,                                          ¬¬     <br />
+                                             ¬      <br />
+  ⁿ                                           |     <br />
+   É                                          ┤     <br />
+  ╢ ⌐                                         ▐     <br />
+       ¬-                                      ⁿ    <br />
+        .f                                      Æ   <br />
+         T  É` I                                J   <br />
+               └.¬.                           ¬     <br />
+                    ╡                 *     ⌐       <br />
+                     É , ┐                ⌐         <br />
+                      `     ∩ ─.-^ -,    ▐          <br />
+                           `         `'¬.[¬         <br />
+                                          `         <br />
+                </div>
+                <div className='ASCIIchars' id='Russia'>
+                                                                ╓ⁿ`1                            <br />
+                                                               ╒Σ, "*Y▄                         <br />
+                                                  £"       ,═^ `"    >╙                         <br />
+                                                  ╙╝     ¿^       ]``                           <br />
+                                                      ¿"        ▀J⌠                             <br />
+                                                     ╛             Y                            <br />
+                                                   ,╙             ▄Ñ⌐"*^`╗                      <br />
+              .                                    M¬w                  ╬                       <br />
+              »``                                 ╟╙^`                  c                       <br />
+                                                 ,╛                      ╥                      <br />
+                           ,═^║                 /                        ╙                      <br />
+                      ,╔X▌║ `             r   ""                          ╟                     <br />
+                     ▐µ  ╓╨              Å                               ╓ⁿ╨                    <br />
+                     ╙ "^ j╦^ⁿ  ,¡╔ ╙*╙                           ╓      H                      <br />
+                       -╕   └╓─`'`▀                              ╙╙v    6N                      <br />
+                             Φ≥                                    ▀ V  ╜                       <br />
+                   ,          @"                                 W ║ ½  *,╫           ≈         <br />
+                ▐┘"` ¥╓  ╓ç,Ω                                  ╒  `"  ¼  ╙,,%       ª╨          <br />
+      ,5╠ «─^%╓.*`    ╙w  ╕                                    └       ╕     `"J⌐               <br />
+      ╚▌ ╙                `                                     $      |       ╙w               <br />
+        %                                                       ╙⌐╓    ,          ╜             <br />
+         ╙w                                                      ╓╙     1         ╕             <br />
+         æ└                                                   «"▓M       *,       *║`           <br />
+        ]Γ                                                  ,Ω             "«,,    ╙%           <br />
+      ╓╜                                                   φ`                  "¬.   ╙          <br />
+       E                                                .*                         `*─H         <br />
+        M                                              ╔                              ▌         <br />
+       ▄└                                              ║                              ╙         <br />
+        ▌                                              ╘                               `        <br />
+         N                                             ║                                        <br />
+         ▄,╓┌                                          ╠        ╓,                              <br />
+            ▀═                                         ▐         ╠M═                     ╗      <br />
+              ╧┐                                       Å ΦM ╗"`"jÜ%  %,                         <br />
+               ▀       ,.,                             w═"╬╗A     `▌─w  *w               Φ      <br />
+                ║     j   ╙╜7,                                     ╙  "w  ╓╜             `      <br />
+                ║   w,H     æ═M                                      \  "╕╙             ▄       <br />
+                "═╛  "      ╡,                                        ╙   "╬w,          ╛       <br />
+                          xΦw╜                                          M   " Å*^      Φ ╕      <br />
+                   ,,m═══*                                               |    ╙                 <br />
+                  Ω`                   ,-^`"``"w                        V                       <br />
+                  ║                   Γ         `\,            /`\       \                      <br />
+                                      "╕           "w,═¬─══   ║  ╙       M                      <br />
+                   Å                   M                   "ⁿ     ╙      /                      <br />
+                  ,M                  ▐                            Ω    ▐                       <br />
+      ¿╕         A                    ╓┴                         /"     \                       <br />
+     ▀  `*≈╥   ¿╨              «═«^*ⁿ"                          ╙w      A                       <br />
+     `^*══w "Y*              ╓⌐                                   'µ╗═ⁿ^                        <br />
+           ╙w,.═"``"^%,,,..^                                       ╢                            <br />
+                </div>
+            </div>
+            <div>
+                Wczytywanie zasobów [###___________________________]
+            </div>
+        </div>
+
+    )
+
+}
+
+export default indexConsole
