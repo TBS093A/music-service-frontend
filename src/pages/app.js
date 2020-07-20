3401c16b2b8de23b885f9461eeeb38802a61de5b
TBS093A <zukkamil.44@gmail.com>
2020-07-20 22:44:54 +0200

    swap MobX to Redux -> delete MobX services

---
+++ src/pages/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 
+import { observer } from 'react-mobx'
+import { render } from 'node-sass'
+
+import Guest from './../stores/guest'
+
+@observer
+class App {
+
+    render() {
+        <div></div>
+    }
+}
+
